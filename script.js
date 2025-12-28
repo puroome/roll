bacce8fee138bce424278b08b7b928c58b1cbd8e
puroome <129413484+puroome@users.noreply.github.com>
2025-12-28 16:42:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ function setupDatePicker() {
           if (!dateStr) return;
 
           if (Object.keys(pendingChanges).length > 0) {
-              showMessageModal("미저장 자료가 있습니다.\n마감하기 전에 저장하세요.");
+              showMessageModal("미저장 자료가 있습니다.\n먼저 저장하세요.");
               instance.setDate(activeDate); 
               updateDateLabel();
               return;
@@ -528,7 +528,7 @@ function renderTable(data) {
 
 async function toggleDateConfirmation(dayStr) {
   if (Object.keys(pendingChanges).length > 0) {
-      showMessageModal("미저장 자료가 있습니다.\n마감하기 전에 저장하세요.");
+      showMessageModal("미저장 자료가 있습니다.\n먼저 저장하세요.");
       const checkbox = document.getElementById('chkConfirmDay');
       checkbox.checked = !checkbox.checked;
       return;
@@ -1674,6 +1674,7 @@ function getRealYear(schoolYear, month) {
   }
   return y;
 }
+
 
 
 
